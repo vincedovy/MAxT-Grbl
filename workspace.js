5511e74ccfd21ea0a71fbff8d3b7cc4e539c4f9a
JPAdie <jpadie@me.com>
2017-01-11 21:30:51 +0000

    Made some changes to ChiliPeppr myWorkspace using Cloud9

--- workspace.js
+++ workspace.js
@@ -1,4 +1,4 @@
-/* global cpdefine chilipeppr cprequire */
+/* global cpdefine chilipeppr cprequire $ */
 cprequire_test(["inline:com-chilipeppr-workspace-grbl"], function(ws) {
 
     console.log("initting workspace");
